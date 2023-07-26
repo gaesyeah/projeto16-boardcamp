e084f292366496676265cb802927e1366d95251c
Gabriel Goes Santos Rios <gabrielgoesrios@gmail.com>
2023-07-26 13:33:46 -0300

    feat: controller games implementado

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -1,14 +1,26 @@
+import { db } from "../database/database.js";
+
 export const selectGames = async (req, res) => {
   try {
-    res.send();
+    const { rows } = await db.query('SELECT * FROM games;');
+    res.send(rows);
   } catch ({ message }) {
     res.status(500).send(message);
   }
 };
 
 export const insertGames = async (req, res) => {
+  const { name, image, stockTotal, pricePerDay } = req.body;
   try {
-    res.send();
+    const { rowCount } = await db.query('SELECT * FROM games WHERE name = $1', [name]);
+    if (rowCount > 0) return res.sendStatus(409);
+
+    await db.query(`
+      INSERT INTO games (name, image, "stockTotal", "pricePerDay") 
+      VALUES ($1, $2, $3, $4)
+      ;` , [name, image, stockTotal, pricePerDay]
+    );
+    res.sendStatus(201);
   } catch ({ message }) {
     res.status(500).send(message);
   }
