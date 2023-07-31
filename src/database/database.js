67f0d33dec39cbe3fc1b5f3f3a632b8fb9189753
Gabriel Goes Santos Rios <gabrielgoesrios@gmail.com>
2023-07-31 12:55:39 -0300

    feat: NODE_ENV: production

--- src/database/database.js
+++ src/database/database.js
@@ -11,6 +11,8 @@ const { Pool } = pg;
 
 export const db = new Pool(configDataBase);
 
+if (process.env.NODE_ENV === "production") configDataBase.ssl = true;
+
 db.connect((error, client, done) => {
   if (error) return console.log('Erro ao conectar ao banco de dados:', error);
   console.log('Conexão bem-sucedida com o banco de dados PostgreSQL');
